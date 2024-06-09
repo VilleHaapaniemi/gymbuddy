5f3cf8fa5d82e74b4aa527e331c115cb6c93108b
Ville Haapaniemi <ville.haapaniemi2@gmail.com>
2024-06-09 15:24:07 +0300

    Removed submodule

---
+++ gymbuddy-user-service/src/main/java/com/vilb/gymbuddy_user_service/GymbuddyUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vilb.gymbuddy_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbuddyUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymbuddyUserServiceApplication.class, args);
+	}
+
+}
